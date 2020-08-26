407d97d36e1faf2c84f60f75a974e1d9ae691de9
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-08-26 16:51:31 +0900

    環境構築

---
+++ src/pages/api/addData/index.js
@@ -0,0 +1,27 @@
+import faunadb from 'faunadb'
+
+// DBアクセス設定
+const secret = process.env.FAUZNABD_SECRET_KEY_NATIONAL_FLAGS
+const q = faunadb.query
+const client = new faunadb. Client({secret})
+
+// 追加データの設定
+import nationalDataAll from '../../../data/nationalData'
+
+export default async (req, res) => {
+    try {
+        const addData = await client.query(
+            q.Map(
+                nationalDataAll,
+                q.Lambda('nationalData', q.Create(q.Collection('national_data'), q.Var('nationalData')))
+            )
+        )
+        // OK時のレスポンス
+        res.status(200).json(addData.data)
+    }
+
+    catch (e) {
+        // エラー時のレスポンス
+        res.status(500).json({error: e.message})
+    }
+}
---
+++ src/pages/api/allData/index.js
@@ -0,0 +1,28 @@
+import faunadb from 'faunadb'
+
+// DBアクセス設定
+const secret = process.env.FAUZNABD_SECRET_KEY_NATIONAL_FLAGS
+const q = faunadb.query
+const client = new faunadb. Client({secret})
+
+export default async (req, res) => {
+    try {
+        const allData = await client.query(
+            q.Map(
+                q.Paginata(
+                    q.Match(
+                        q.Index('all_national_data')
+                    )
+                ),
+                ref => q.Get(ref)
+            )
+        )
+        // OK時のレスポンス
+        res.status(200).json(allData.data)
+    }
+
+    catch (e) {
+        // エラー時のレスポンス
+        res.status(500).json({error: e.message})
+    }
+}
