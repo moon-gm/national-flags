5fa14837a5d035220e01243ae342be59800373a0
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-08-29 01:13:47 +0900

    修正

--- src/pages/centralAmerica.js
+++ src/pages/centralAmerica.js
@@ -2,54 +2,14 @@ import { useEffect, useState} from 'react'
 import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
 
-export default function Home() {
+export default function CentralAmerica() {
   useEffect(()=> {
     selectGroup('centralAmerica')
   ,[]})
 
-  // 国データをnationalDataに入れて一時管理
-  const [nationalData, setNationalData] = useState([])
-
-  // DBデータをワード検索して取得する処理
-  async function searchTerm () {
-    // formのinputでname属性が[serchWord]の要素を取得
-    const elements = document.formOfSearch.searchWord
-
-    // 選択されたラジオボタンのvalueをselectValueに代入
-    for (var i = 0; i < elements.length; i++) {
-      if (elements[i].checked){
-        var selectValue = elements[i].value
-      }
-    }
-
-    // 入力値の取得
-    const searchTerm = document.getElementById('searchByName').value
-
-    // selectValueの値によって処理を分ける
-    if (selectValue === "nationalName") {
-
-      // 国名検索の場合
-      const res = await fetch(`/api/search/byName/${searchTerm}`)
-      const searchData = await res.json()
-      setNationalData(searchData)
-
-    } else if (selectValue === "capital") {
-
-      // 首都名検索の場合
-      const res = await fetch(`/api/search/byCapital/${searchTerm}`)
-      const searchData = await res.json()
-      setNationalData(searchData)
-
-    } else if (selectValue === "currency") {
-
-      // 通貨名検索の場合
-      const res = await fetch(`/api/search/byCurrency/${searchTerm}`)
-      const searchData = await res.json()
-      setNationalData(searchData)
-      
-    }
-  }
-
+    // 国データをnationalDataに入れて一時管理
+    const [nationalData, setNationalData] = useState([])
+    
   async function selectGroup (selectGroup) {
     const res = await fetch(`/api/search/byGroup/${selectGroup}`)
     const groupData = await res.json()
@@ -58,17 +18,6 @@ export default function Home() {
 
   return (
     <>
-
-      <div className="searchArea">
-          <form name="formOfSearch">
-            <lavel><input type="radio" name="searchWord" id="nationalName" value="nationalName"/>国名（カタカナ or 漢字）</lavel>
-            <lavel><input type="radio" name="searchWord" id="capital" value="capital"/>首都名（カタカナ or 漢字）</lavel>
-            <lavel><input type="radio" name="searchWord" id="currency" value="currency"/>通貨名（カタカナ or 漢字）</lavel>
-            <input type="text" name="searchByName" id="searchByName" placeholder="検索種別にチェックを入れて入力"/>
-            <input type="button" onClick={searchTerm} value="Search"/>
-          </form>
-      </div>
-
       <div className={styles.grid}>
         <Link href="/asia" className={styles.card}>
           <h3>アジア &rarr;</h3>
