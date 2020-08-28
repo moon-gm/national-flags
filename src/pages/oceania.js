8af0be8714ecf45027f3eb727d62a9ee1279c418
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-08-29 02:15:33 +0900

    修正

--- src/pages/oceania.js
+++ src/pages/oceania.js
@@ -1,34 +1,7 @@
-import { useEffect, useState} from 'react'
-import DataBox from '../components/dataBox'
-import Navigation from '../components/navigation'
+import GroupPage from '../components/groupPage'
 
 export default function Oceania() {
-  useEffect(()=> {
-    selectGroup('oceania')
-  ,[]})
-
-  // 国データをnationalDataに入れて一時管理
-  const [nationalData, setNationalData] = useState([])
-
-  async function selectGroup (selectGroup) {
-    const res = await fetch(`/api/search/byGroup/${selectGroup}`)
-    const groupData = await res.json()
-    setNationalData(groupData)
-  }
-
   return (
-    <>
-     <Navigation/>
-
-      {nationalData.length > 0 ? (
-          nationalData.map(d => (
-            <DataBox d={d}/>
-          ))
-        ) : (
-          <div>
-            Loading...
-          </div>
-        )}
-    </>
+    <GroupPage group="oceania"/>
   )
 }
