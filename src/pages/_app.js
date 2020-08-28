e2db645347d37ba0c684b66502e9bb3c3fab068d
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-08-28 18:32:07 +0900

    各地域２国j情報追加 / 画像追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 
+const Layout = ({children}) => {
+  return (
+    <>
+      {/* ヘッド情報 */}
+      <Head>
+        <title>National Flags</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* ヘッダーエリア */}
+      <header>National Flags</header>
+
+      {/* メインエリア */}
+      <main>{children}</main>
+
+      {/* フッターエリア */}
+      <footer>Powered by National Flags</footer>
+    </>
+  )
+}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import {useEffect, useState} from 'react'
 import styles from '../styles/Home.module.scss'
 
@@ -63,18 +62,10 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <main className={styles.main}>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <div className="flag-area">
-            <p>DBのデータを更新<button onClick={update}>Update</button></p>
+      <div className="flag-area">
+          <p>DBのデータを更新<button onClick={update}>Update</button></p>
           <form name="formOfSearch">
             <lavel><input type="radio" name="searchWord" id="nationalName" value="nationalName"/>国名（カタカナ or 漢字）</lavel>
             <lavel><input type="radio" name="searchWord" id="capital" value="capital"/>首都名（カタカナ or 漢字）</lavel>
@@ -82,118 +73,140 @@ export default function Home() {
             <input type="text" name="searchByName" id="searchByName" placeholder="検索種別にチェックを入れて入力"/>
             <input type="button" onClick={searchTerm} value="Search"/>
           </form>
+      </div>
 
-        </div>
-        {
-          nationalData.length > 0 ? (
-            nationalData.map(d => (
-              <div className="flag-area">
-                <p className="p" id={d.data.id}>
-                  {d.data.id}
-                </p>
-                <p className="p">
-                  {d.data.group.id}
-                </p>
-                <p className="p">
-                  {d.data.group.name}
-                </p>
-                <p className="p">
-                  {d.data.name.katakana}
-                </p>
-                <p className="p">
-                  {d.data.name.kanji}
-                </p>
-                <p className="p">
-                  {
-                    d.data.language.map(l => (
-                      `${l} / `
-                    ))
-                  }
-                </p>
-                <p className="p">
-                  {d.data.currency}
-                </p>
-                <p className="p">
-                  {d.data.capital}
-                </p>
-                <p className="p">
-                  {d.data.area}k㎡
-                </p>
-                <p className="p">
-                  {d.data.population}人
-                </p>
-                <p className="p">
-                  {d.data.timeLag}時間
-                </p>
-                <p className="p">
-                  {d.data.sinse}年
-                </p>
-                <p className="p">
-                  {d.data.origin.name}
-                </p>
-                <p className="p">
-                  {d.data.origin.flag}
-                </p>
-                <p className="p">
-                  {d.data.knowledge.title}
-                </p>
-                <p className="p">
-                  {d.data.knowledge.contents}
-                </p>
-              </div>
-            ))
-          ) : (
-            <div>
-              Loading...
+      {nationalData.length > 0 ? (
+          nationalData.map(d => (
+            <div className="flag-area">
+              <p className="p" id={d.data.id}>
+                {d.data.id}
+                <img src={`/${d.data.id}.png`} alt={d.data.name.katakana}/>
+              </p>
+              <p className="p">
+                {d.data.group.id}
+              </p>
+              <p className="p">
+                {d.data.group.name}
+              </p>
+              <p className="p">
+                {d.data.name.katakana}
+              </p>
+              <p className="p">
+                {d.data.name.official}
+              </p>
+              <p className="p">
+                {d.data.name.kanji}
+              </p>
+              <p className="p">
+                {
+                  d.data.language.map(l => (
+                    `${l} / `
+                  ))
+                }
+              </p>
+              <p className="p">
+                {d.data.currency}
+              </p>
+              <p className="p">
+                {d.data.capital}
+              </p>
+              <p className="p">
+                {d.data.area}k㎡
+              </p>
+              <p className="p">
+                {d.data.population}人
+              </p>
+              <p className="p">
+                {d.data.timeLag}時間
+              </p>
+              <p className="p">
+                {d.data.sinse}年
+              </p>
+              <p className="p">
+                {d.data.origin.name}
+              </p>
+              <p className="p">
+                {d.data.origin.flag}
+              </p>
+              <p className="p">
+                {d.data.knowledge.title}
+              </p>
+              <p className="p">
+                {d.data.knowledge.contents}
+              </p>
             </div>
-          )
-        }
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          ))
+        ) : (
+          <div>
+            Loading...
+          </div>
+        )}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      <div className={styles.grid}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h3>アジア &rarr;</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
         </a>
-      </footer>
-    </div>
+
+        <a href="https://nextjs.org/learn" className={styles.card}>
+          <h3>ヨーロッパ &rarr;</h3>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
+
+        <a
+          href="https://github.com/vercel/next.js/tree/master/examples"
+          className={styles.card}
+        >
+          <h3>中東 &rarr;</h3>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h3>アフリカ &rarr;</h3>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+        <a
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h3>オセアニア &rarr;</h3>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+        <a
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h3>北アメリカ &rarr;</h3>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+        <a
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h3>中央アメリカ &rarr;</h3>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+        <a
+          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h3>南アメリカ &rarr;</h3>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   )
 }
