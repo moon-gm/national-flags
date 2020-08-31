e79f18451589b6e491aeac69a1b21036a5b9f636
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-08-31 12:14:22 +0900

    ローカルのenv設定処理追加

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
 	cssModules: true,
-});
+	// ローカル開発時ここにDBアクセスキーを入力し、コメントアウトを外す(KeyはPUSHしないこと)
+	// env: {
+	// 	"FAUNADB_SECRET_KEY_NATIONAL_FLAGS": ""
+	// }
+})
