0176d0ec87ccd0e6b41ce2b3dfd462b899bec7ee
akinaga <60134740+akinaga-hikaru@users.noreply.github.com>
2020-10-06 15:47:16 +0900

    完了画面にTop画面への導線追加

--- src/pages/setting/register/complete.js
+++ src/pages/setting/register/complete.js
@@ -1,14 +1,22 @@
+import Link from 'next/link'
 import styles from '../../../styles/pages/register.module.scss'
 
-export default function Complete(state) {
+export default function Complete() {
 
 // -------------------- 入力完了ページ --------------------
 	return (
-		<div className={styles.grid}>
-			<h1 className={styles.title}>
-				データ登録完了
-			</h1>
-		</div>
+		<>
+			<div className={styles.grid}>
+				<h1 className={styles.title}>
+					データ登録完了
+				</h1>
+				<Link href="/">
+					<button className={styles.commonBtn}>
+						Top画面へ
+					</button>
+				</Link>
+			</div>
+		</>
 	)
 
 }
